68bf62dac6801c15c1b88f45afabe3070556cb19
kawis <krzymado@gmail.com>
2024-01-19 18:30:01 +0100

    Added search functionality

--- src/components/DrinkCard.jsx
+++ src/components/DrinkCard.jsx
@@ -1,13 +1,23 @@
 export function DrinkCard(props) {
     return (
-        <div class="max-w-sm rounded-lg border-black border-8 bg-red-500">
+        <div className={`shadow-lg max-w-sm min-w-sm h-96 w-96 rounded-lg border-black border-8`}>
+            <div className='flex flex-col content-end h-full bg-cover bg-[image:var(--image-url)]'
+                 style={{'--image-url': `url(${props.image})`}} >
 
-            <div className="basis-3/4">
-                <img src={props.image} className="object-contain"/>
-            </div>
+                <div className="basis-3/4">
+                </div>
+                <div className="basis-1/4 backdrop-blur-lg bg-white/70 p-2 w-full text-xl text-center">
 
-            <p class="p-2 text-xl text-center bg-secondary-color">{props.name}</p>
-               
+                    <p className="text-3xl mt-2">{props.name}</p>
+
+                    <div className="flex flex-row justify-between">
+                        <p className="m-3">{props.category}</p>
+                        <p className="m-3">{props.glass}</p>
+                    </div>
+                   
+                </div>
+                
+            </div>    
         </div>
     )
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -6,23 +6,46 @@ import axios from "axios";
 export function Search() {
 
     const [items, setItems] = useState([]);
+    const [input, setInput] = useState("");
 
     useEffect(() => {
-      axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
-        .then(response => {
-          setItems(response.data.drinks);
-        })
-        .catch(error => {
-          console.error(error);
+      const delayDebounceFn = setTimeout(() => {
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`)
+          .then(response => {
+            if(response.data.drinks != null)
+              setItems(response.data.drinks);
+            else
+              setItems([]);
+          })
+          .catch(error => {
+            console.error(error);
         });
-    }, []);
+      }, 300)
+
+      return () => clearTimeout(delayDebounceFn)
+    }, [input]);
+    
 
     return(
-      <div className="flex flex-row flex-wrap justify-around gap-3 p-10 bg-background-color">
-        {items.map(item => (
-          <DrinkCard key={item.idDrink} name={item.strDrink} image={item.strDrinkThumb}/>
-        ))}
+      <div className="flex flex-col bg-background-color">
+
+        <div className="my-10 w-5/12 mx-auto">
+          <input
+            type="text" 
+            placeholder="Search for a drink..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            class="py-2 px-1 w-full bg-secondary-color rounded-lg border-2 placeholder-gray-300 text-white"
+          />
+        </div>
+       
+        <div className="flex flex-row flex-wrap justify-around gap-3 p-10">
+          {items.map(item => (
+            <DrinkCard key={item.idDrink} name={item.strDrink} image={item.strDrinkThumb} glass={item.strGlass} category={item.strCategory}/>
+          ))}
+        </div>
       </div>
+      
     )
   }
   
